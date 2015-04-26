ec8301b30d74e4ceffc80f7759c3588b272b5d4d
Marat Abdullin <dakota@brokenpipe.ru>
2015-04-26 22:32:37 +0300

    Custom test runner.

---
+++ t/testrunner.js
@@ -0,0 +1,23 @@
+var spawn = require('child_process').spawn;
+var path = require('path');
+
+var testServer = spawn(
+    './iojs/iojs',
+    ['./t/testserver.js', './t/testrunner.json'],
+    {stdio: 'inherit', env: process.env}
+);
+
+var testRunnerEnv = JSON.parse(JSON.stringify(process.env));
+testRunnerEnv.PATH = (testRunnerEnv.PATH ? testRunnerEnv.PATH + ':' : '') + '../dosido/nginx/objs';
+testRunnerEnv.TEST_NGINX_SERVROOT = path.join(path.resolve('.'), '..', 'dosido/t/servroot');
+
+var testRunner = spawn(
+    'prove',
+    ['-r', '../dosido/t'],
+    {stdio: 'inherit', env: testRunnerEnv, cwd: '../test-nginx'}
+);
+
+testRunner.on('close', function(code) {
+    testServer.kill();
+    process.exit(code);
+});
